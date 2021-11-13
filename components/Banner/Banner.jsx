c19720ceb92495f8c5a756fd666515171d2329ef
Vijay0126 <84371499+Vijay0126@users.noreply.github.com>
2021-11-13 19:04:13 +0530

    Add files via upload

---
+++ components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <>
+        <div className="mx-6 py-6 px-12 mt-4 mb-4">
+        <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/lead-in-v3-collection-202102040828.png"
+        className="w-full" 
+        alt="banner"
+        />       
+        </div>
+        </>
+    )
+}
+
+export default Banner
